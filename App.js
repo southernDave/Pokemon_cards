d7781deaf0b6d96860c3056010719202294a68ad
Southerndave <obohdavid2014@gmail.com>
2024-03-02 12:22:07 -0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Platform, ScrollView } from 'react-native';
+import PokemonCard from './components/PokemonCard';
 
 export default function App() {
+  
+  const charmanderData = {
+      name: "charmander",
+      image: require("./assets/charmander.jpg"),
+      type: "Fire",
+      hp: 39,
+      moves: ["Scratch", "Ember", "Growl", "Leer"],
+      weaknesses: ["Water", "Rock"]
+  }
+
+  const squirtleData = {
+    name: "squirtle",
+    image: require("./assets/squirtle.jpg"),
+    type: "Water",
+    hp: 44,
+    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+    weaknesses: ["Electric", "Grass"]
+  }
+
+  const bulsbasaurData = {
+    name: "bulsbasaur",
+    image: require("./assets/bulsbasaur.jpg"),
+    type: "Grass",
+    hp: 45,
+    moves: ["Tackles", "Vine Whip", "Growl", "Leech Seed"],
+    weaknesses: ["Fire", "Ice", "Flying", "Psychic"]
+  }
+
+  const pikachuData = {
+    name: "pikachu",
+    image: require("./assets/pikachu.jpg"),
+    type: "Electric",
+    hp: 35,
+    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+    weaknesses: ["Ground"]
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <PokemonCard {...charmanderData} />
+        <PokemonCard {...squirtleData} />
+        <PokemonCard {...bulsbasaurData} />
+        <PokemonCard {...pikachuData} />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
   },
 });
